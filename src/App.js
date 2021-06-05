7ba9e9bdeccedad4aec9dd76a18798c6c1fc2377
Cindy Ravell <cindy.ravell@theksquaregroup.com>
2021-06-04 23:49:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import CreateMovie from "./create-movie-fn";
+import MoviesList from "./movies-list-fn";
 
-function App() {
+/** Copied from React docs, don't touch!!!! */
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const languages = {
+  english: "en/us",
+  spanish: "es/mx"
+};
+
+export const ThemeContext = React.createContext(themes.light);
+export const LanguageContext = React.createContext(languages.english);
+
+export default function App() {
+  const [createdMovies, setCreatedMovies] = useState([]);
+  function createMovieFn(newMovie) {
+    setCreatedMovies([...createdMovies, newMovie]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={themes.dark}>
+      <LanguageContext.Provider value={languages.spanish}>
+        <div className="App">
+          <MoviesList additionalMovies={createdMovies} />
+          <CreateMovie createMovieFn={createMovieFn} />
+        </div>
+      </LanguageContext.Provider>
+    </ThemeContext.Provider>
   );
 }
-
-export default App;
